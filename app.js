a1d0afd1d5280d074d2b96892f7b398bdeed31ca
tyhsu <yardan.xii@gmail.com>
2021-06-03 20:54:34 +0800

    added show page

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
 })
 
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  console.log('req.params.restaurant_id', req.params.restaurant_id)
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
