7b40d9539901e0b25966ccf7e63216eaaa51e9c7
tyhsu <yardan.xii@gmail.com>
2021-06-03 21:02:57 +0800

    added search function

--- app.js
+++ app.js
@@ -26,6 +26,14 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants })
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
