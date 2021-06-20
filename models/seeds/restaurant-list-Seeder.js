be2111beacfcc2a58c2272e0c7d0e0baca4673b1
tyhsu <yardan.xii@gmail.com>
2021-06-20 18:57:56 +0800

    + seeder

---
+++ models/seeds/restaurant-list-Seeder.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Restaurant = require('../restaurant-list.js') // 載入 restaurant model
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+    for (let i = 0; i < 10; i++) {
+      Restaurant.create({ name: 'name-' + i })
+    }
+    console.log('done')
+  })
