3047e3596030f8269e4e8cdf312bb0ad00443e3e
tyhsu <yardan.xii@gmail.com>
2021-06-21 08:09:25 +0800

    + seeder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const Restaurant = require('./models/restaurant')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant') // 載入 restaurant model
+const restData = require('../../restaurants.json')
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -10,10 +11,21 @@ db.on('error', () => {
 })
 db.once('open', () => {
     console.log('mongodb connected!')
+    const restArray = restData.results
 
-    for (let i = 0; i < 10; i++) {
-      Restaurant.create({ name: 'name-' + i })
-    }
-    
+  for (let i = 0; i < restArray.length; i++) {
+    Restaurant.create({
+      name: restArray[i].name,
+      name_en: restArray[i].name_en,
+      category: restArray[i].category,
+      image: restArray[i].image,
+      location: restArray[i].location,
+      phone: restArray[i].phone,
+      google_map: restArray[i].google_map,
+      rating: restArray[i].rating,
+      description: restArray[i].description
+    })
+  }
+
     console.log('done')
   })
