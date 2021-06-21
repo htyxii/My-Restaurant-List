794d7444583c3c78efe04fcb76e26296171b4436
tyhsu <yardan.xii@gmail.com>
2021-06-21 09:04:27 +0800

    fixed router

--- app.js
+++ app.js
@@ -30,8 +30,10 @@ app.use(express.static('public'))
 
 // routes setting
 app.get('/', (req, res) => {
-  // past the restaurant data into 'index' partial template
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find() // 取出 Restaurant model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(restaurants => res.render('index', { restaurants })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
 })
 
 app.get('/restaurants/:restaurant_id', (req, res) => {
@@ -39,12 +41,17 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
+
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter(restaurant => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
   })
   res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
 })
 
 // start and listen on the Express server
